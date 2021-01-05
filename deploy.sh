f7a26553a554ec177a5031b97fc0401c49b878dc
GowthamYadav <gowtham.b.yadav@gmail.com>
2021-01-05 20:26:52 +0530

    adding deploy script and k8s config changes

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# build images
+docker build -t gowthamyadav/multi-client:latest -t gowthamyadav/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t gowthamyadav/multi-server:latest -t gowthamyadav/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t gowthamyadav/multi-worker:latest  -t gowthamyadav/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# push images to dockerhub
+docker push gowthamyadav/multi-client:latest
+docker push gowthamyadav/multi-server:latest
+docker push gowthamyadav/multi-worker:latest
+
+docker push gowthamyadav/multi-client:$SHA
+docker push gowthamyadav/multi-server:$SHA
+docker push gowthamyadav/multi-worker:$SHA
+
+# apply k8s config files for deployment and services
+kubectl apply -f k8s
+
+# upgrader images to latest
+kubectl set image deployment/client-deployment client=gowthamyadav/multi-client:$SHA
+kubectl set image deployment/server-deployment client=gowthamyadav/multi-server:$SHA
+kubectl set image deployment/worker-deployment client=gowthamyadav/multi-worker:$SHA
